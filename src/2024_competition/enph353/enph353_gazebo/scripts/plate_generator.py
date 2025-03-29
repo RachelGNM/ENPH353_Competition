c8844e98f736c136eba3cccd80282ea9fea26cdb
Alfred <you@example.com>
2025-03-28 20:42:06 -0700

    Modified plate generation to take clues from clues.csv and added several starting clue and image datasets

--- src/2024_competition/enph353/enph353_gazebo/scripts/plate_generator.py
+++ src/2024_competition/enph353/enph353_gazebo/scripts/plate_generator.py
@@ -35,7 +35,11 @@ def loadCrimesProfileCompetition():
 
     # We will save the clues to plates.csv
     # TODO Rename plates.csv to clues.csv
+<<<<<<< HEAD
     with open(SCRIPT_PATH + "plates.csv", 'w') as plates_file:
+=======
+    with open(SCRIPT_PATH + "clues.csv", 'w') as plates_file:
+>>>>>>> Modified plate generation to take clues from clues.csv and added several starting clue and image datasets
         csvwriter = csv.writer(plates_file)
 
         for (key, value) in zip(key_list, value_list):
@@ -46,6 +50,22 @@ def loadCrimesProfileCompetition():
 
     return clues
 
+<<<<<<< HEAD
+=======
+def loadClues():
+    script_path = os.path.dirname(os.path.realpath(__file__)) + "/"
+    clues = {}
+
+    # Read from clues.csv
+    with open(script_path + "clues.csv", 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            key, value = row
+            clues[key] = value
+
+    return clues
+
+>>>>>>> Modified plate generation to take clues from clues.csv and added several starting clue and image datasets
 # Find the path to this script
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
 TEXTURE_PATH = '../media/materials/textures/'
@@ -55,7 +75,11 @@ PLATE_HEIGHT = 600
 PLATE_WIDTH = banner_canvas.shape[1]
 IMG_DEPTH = 3
 
+<<<<<<< HEAD
 clues = loadCrimesProfileCompetition()
+=======
+clues = loadClues()
+>>>>>>> Modified plate generation to take clues from clues.csv and added several starting clue and image datasets
 
 i = 0
 for key, value in clues.items():
